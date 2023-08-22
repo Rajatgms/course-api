77d30df91451165710ac4a232363dc920a54189a
“Rajat <23decrajat@gmail.com>
2023-08-22 13:23:27 -0400

    Create Topic MVC - getAllTopics

---
+++ src/main/java/com/example/courseapi/topic/TopicService.java
@@ -0,0 +1,22 @@
+package com.example.courseapi.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = new ArrayList<>(
+            Arrays.asList(
+                    new Topic("1", "TopicName1", "TopicDescription1"),
+                    new Topic("2", "TopicName2", "TopicDescription2")
+            )
+    );
+
+    public List<Topic> getAllCourses() {
+        return topics;
+    }
+}
