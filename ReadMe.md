7f7bdfecbc7c84b43cb17ecc19d4c5558f68153c
“Rajat <23decrajat@gmail.com>
2023-08-22 21:00:56 -0400

    Use custom query to getCourseByTopicId

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+#Java Spring Boot Application
+- Tech - Spring Boot, JPA, and Derby database
+- APIs
+  - `/topics` - CRUD
+  - `/courses` - CRUD
+  - Courses and Topics has one-to-many relationship
--- src/main/java/com/example/courseapi/course/CourseRepository.java
+++ src/main/java/com/example/courseapi/course/CourseRepository.java
@@ -1,5 +1,6 @@
 package com.example.courseapi.course;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -7,5 +8,6 @@ import java.util.List;
 
 public interface CourseRepository extends CrudRepository<Course, String> {
 
+    @Query("SELECT c FROM Course c WHERE c.topic.id =:id")
     public List<Course> findByTopicId(@Param("id") String id);
 }
